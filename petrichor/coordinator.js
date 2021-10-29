5a49b7b75d9eca7aa345729cb922fd88aea75c6b
Alex Drover <alexfdrover@gmail.com>
2021-10-28 17:01:32 -0700

    Create petrichor dir for coordinator and config. Add command for running multiple instances of runner.js
    Co-authored-by: Raul de Hevia <raul.dehevia@gmail.com>
    Co-authored-by: Stephanie Cunnane <stephanie.cunnane@gmail.com>
    Co-authored-by: Minh Phan Hvu <minhphanh.vu@gmail.com>

---
+++ petrichor/coordinator.js
@@ -0,0 +1,76 @@
+const { exec } = require('child_process');
+/*
+MENTAL MODEL:
+ - coordinator tells config.js to fetch script and config from s3 bucket
+ - Start X instances of runner and 1 instance of normalizer
+*/
+
+/*
+START_RAIN or START_STORM - Petrichor
+  - Gets test script from location (S3 prod, local file test)
+  - Gets total test length from location (S3 prod, local file test)
+  - Gets Origin Timestamp from location (S3 prod, local file test)
+  - Gets number of users from location (S3 prod, local file test)
+  - Start X instances of runner and 1 instance of normalizer
+
+const option1 = {
+  stepName: "Load Main Page",
+  userId: "34f3n98",
+  stepStartTime: 134,
+  responseTime: 900,
+};
+
+*/
+
+async function fetchScript() {
+
+}
+
+async function fetchConfig() {
+  return 
+}
+
+(async () => {
+  const script = await fetchScript();
+  const config = await fetchConfig();
+  // save to folder
+})();
+
+
+const executeCommand = (cmd, successCallback, errorCallback) => {
+  exec(cmd, (error, stdout, stderr) => {
+    if (error) {
+     // console.log(`error: ${error.message}`);
+      if (errorCallback) {
+        errorCallback(error.message);
+      }
+      return;
+    }
+    if (stderr) {
+      //console.log(`stderr: ${stderr}`);
+      if (errorCallback) {
+        errorCallback(stderr);
+      }
+      return;
+    }
+    //console.log(`stdout: ${stdout}`);
+    if (successCallback) {
+      successCallback(stdout);
+    }
+  });
+};
+
+const startProcess = (numberOfUsers=5, success, error) => {
+  let command = "";
+  for (let i = 0; i < numberOfUsers; i++) {
+    command = command + "node runner.js & ";
+  }
+  command += "node normalizer.js";
+  executeCommand(
+    command,
+    branch => success(branch),
+    errormsg => error(errormsg)
+  );
+}; 
+
+startProcess();
