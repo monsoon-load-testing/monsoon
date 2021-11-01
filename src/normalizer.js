63392c5590995d0e8c56df285e4c9b7c758547fa
Stephanie Cunnane <stephanie.cunnane@gmail.com>
2021-11-01 14:05:01 -0700

    Finish re-centering of window
    Co-authored-by: Minh Vu <minhphanh.vu@gmail.com>
    Co-authored-by: Alex Drover <alexfdrover@gmail.com>
    Co-authored-by: Raul de Hevia <raul.dehevia@gmail.com>

--- src/normalizer.js
+++ src/normalizer.js
@@ -1,128 +1,173 @@
 const fs = require("fs");
-
-const originTimestamp = 1635530769000; // hard-coded for dummyResults
-
-// window is 2 seconds, testDuration is 10s
-const timeWindow = 2000; // 2s
-const testDuration = 10_000; // 10s
-
-const initializeTimestamps = (timeWindow, testDuration, originTimestamp) => {
-  let currentTime = originTimestamp;
-  const normalizedTimestamps = [];
-  const finalTimestamp = originTimestamp + testDuration;
-  while (currentTime < finalTimestamp) {
-    normalizedTimestamps.push(currentTime);
-    currentTime += timeWindow;
-  }
-  normalizedTimestamps.push(finalTimestamp);
-  return normalizedTimestamps;
-};
-
-const normalizedTimestamps = initializeTimestamps(
-  timeWindow,
-  testDuration,
-  originTimestamp
-);
-console.log({ normalizedTimestamps });
-
-// make the buckets object
-let buckets = {};
-normalizedTimestamps.forEach((normalizedTimestamp) => {
-  buckets[normalizedTimestamp] = [];
-});
+const AWS = require("aws-sdk");
 
 (async () => {
-  try {
-    const filenames = await fs.promises.readdir("./results");
-    filenames.forEach(async (filename) => {
-      const stepName = filename.split("-")[1];
+  // Faking the polling logic where we reach out to the results folder
+  const sleep = (ms) => {
+    return new Promise((resolve) => setTimeout(resolve, ms));
+  };
 
-      let fileContents = await fs.promises.readFile(`./results/${filename}`, {
-        encoding: "utf-8",
-      });
-      fileContents = JSON.parse(fileContents);
-      fileContents.stepName = stepName;
-      for (let idx = 1; idx < normalizedTimestamps.length; idx++) {
-        if (
-          fileContents.stepStartTime >= normalizedTimestamps[idx - 1] &&
-          fileContents.stepStartTime < normalizedTimestamps[idx]
-        ) {
-          let normalizedTimestamp = normalizedTimestamps[idx - 1];
-          buckets[normalizedTimestamp].push(fileContents);
-        }
-      }
-    });
-  } catch (err) {
-    console.error(err);
-  }
-})();
+  await sleep(5_000);
 
-setTimeout(() => {
-  downsample();
-}, 3000);
+  const originTimestamp = 1635530769000; // hard-coded for dummyResults
 
-let finalBucket;
-const downsample = () => {
-  const filteredBucket = filterBucket(); // based on same userId - same stepName
-  buildFinalBucket(filteredBucket);
-  console.log(filteredBucket);
-};
+  // window is 2 seconds, testDuration is 10s
+  const timeWindow = 2000;
+  const testDuration = 10_000;
 
-const filterBucket = () => {
-  const filteredBucket = {};
-  normalizedTimestamps.forEach((normalizedTimestamp) => {
-    if (buckets[normalizedTimestamp].length === 0) {
-      filteredBucket[normalizedTimestamp] = [];
+  const initializeTimestamps = (timeWindow, testDuration, originTimestamp) => {
+    let currentTime = originTimestamp;
+    const normalizedTimestamps = [];
+    const finalTimestamp = originTimestamp + testDuration;
+    while (currentTime < finalTimestamp) {
+      normalizedTimestamps.push(currentTime);
+      currentTime += timeWindow;
     }
-    buckets[normalizedTimestamp].forEach((dataPoint) => {
-      const key = `${dataPoint.userId}-${dataPoint.stepName}-${String(
-        normalizedTimestamp
-      )}`;
-      if (filteredBucket[key]) {
-        filteredBucket.push(dataPoint);
-      } else {
-        filteredBucket[key] = [dataPoint];
-      }
-    });
-  });
-  return filteredBucket;
-};
+    normalizedTimestamps.push(finalTimestamp);
+    return normalizedTimestamps;
+  };
 
-const buildFinalBucket = (filteredBucket) => {
-  Object.keys(filteredBucket).forEach((key) => {
-    let sum = 0;
-    filteredBucket[key].forEach((dataPoint) => {
-      sum += dataPoint.metrics.responseTime;
+  const normalizedTimestamps = initializeTimestamps(
+    timeWindow,
+    testDuration,
+    originTimestamp
+  );
+
+  // make the buckets object
+  let buckets = {};
+  normalizedTimestamps.forEach((normalizedTimestamp) => {
+    buckets[normalizedTimestamp] = [];
+  });
+
+  (async () => {
+    try {
+      const filenames = await fs.promises.readdir("./results");
+      filenames.forEach(async (filename) => {
+        const stepName = filename.split("-")[1];
+
+        let fileContents = await fs.promises.readFile(`./results/${filename}`, {
+          encoding: "utf-8",
+        });
+        fileContents = JSON.parse(fileContents);
+        fileContents.stepName = stepName;
+        for (let idx = 0; idx < normalizedTimestamps.length; idx++) {
+          const [lowerBound, upperBound] = [
+            normalizedTimestamps[idx] - timeWindow / 2,
+            normalizedTimestamps[idx] + timeWindow / 2,
+          ];
+          if (
+            fileContents.stepStartTime >= lowerBound &&
+            fileContents.stepStartTime < upperBound
+          ) {
+            let normalizedTimestamp = normalizedTimestamps[idx];
+            buckets[normalizedTimestamp].push(fileContents);
+          }
+        }
+      });
+    } catch (err) {
+      console.error(err);
+    }
+  })();
+
+  let finalBucket = {};
+  const downsample = () => {
+    const filteredBucket = filterBucket(); // based on same userId - same stepName
+    buildFinalBucket(filteredBucket);
+  };
+
+  const filterBucket = () => {
+    const filteredBucket = {};
+    normalizedTimestamps.forEach((normalizedTimestamp) => {
+      if (buckets[normalizedTimestamp].length === 0) {
+        filteredBucket[normalizedTimestamp] = [];
+      }
+      buckets[normalizedTimestamp].forEach((dataPoint) => {
+        const key = `${dataPoint.userId}-${dataPoint.stepName}-${String(
+          normalizedTimestamp
+        )}`;
+        if (filteredBucket[key]) {
+          filteredBucket.push(dataPoint);
+        } else {
+          filteredBucket[key] = [dataPoint];
+        }
+      });
     });
-    const normalizedResponseTime = sum / filteredBucket[key].length;
-    const [userId, stepName, normalizedTimestamp] = key.split("-");
-    const newKey = `${stepName}-${normalizedTimestamp}`;
-    if (filteredBucket[key].length === 0) {
-      finalBucket[key] = {};
-    } else if (finalBucket[newKey]) {
-      // if newKey already exists
-      finalBucket[newKey][userId] = {
-        metrics: {
-          normalizedResponseTime,
-        },
-      };
-    } else {
-      // newKey doesn't exist yet
-      finalBucket[newKey] = {
-        [userId]: {
+    return filteredBucket;
+  };
+
+  const buildFinalBucket = (filteredBucket) => {
+    Object.keys(filteredBucket).forEach((key) => {
+      let sum = 0;
+      filteredBucket[key].forEach((dataPoint) => {
+        sum += dataPoint.metrics.responseTime;
+      });
+      const normalizedResponseTime = sum / filteredBucket[key].length;
+      const [userId, stepName, normalizedTimestamp] = key.split("-");
+      const newKey = `${stepName}-${normalizedTimestamp}`;
+
+      if (filteredBucket[key].length === 0) {
+        finalBucket[key] = {};
+      } else if (finalBucket[newKey]) {
+        // if newKey already exists
+        finalBucket[newKey][userId] = {
           metrics: {
             normalizedResponseTime,
           },
-        },
-      };
-    }
-  });
-  console.log(finalBucket);
-};
+        };
+      } else {
+        // newKey doesn't exist yet
+        finalBucket[newKey] = {
+          [userId]: {
+            metrics: {
+              normalizedResponseTime,
+            },
+          },
+        };
+      }
+    });
+    console.log("final bucket", finalBucket);
+  };
 
-/*
-Goal: convert finalBucket to a bunch of JSON files:
-          finalBucket key -> filename
-          finalBucket value -> file contents
-      send it to a temporary folder dummyS3
-*/
+  const writeToS3 = () => {
+    const BUCKET_NAME = "monsoon-load-testing-bucket";
+    // write to local dummyS3
+    /*
+    for (let filename in finalBucket) {
+      path = `./dummyS3/${filename}.json`;
+      fs.writeFile(
+        path,
+        JSON.stringify(finalBucket[filename]),
+        { encoding: "utf-8" },
+        (err) => {
+          if (err) {
+            console.log(err);
+          } else {
+            console.log(`bucket ${filename} is stored.`);
+          }
+        }
+      );
+    }
+    */
+    // Access to s3 bucket from BUCKET_NAME
+    // from AWS credentials, we retrieve the existing s3 bucket inside the s3 object
+    for (let filename in finalBucket) {
+      const params = {
+        Bucket: BUCKET_NAME,
+        Key: `${filename}.json`, // File name you want to save as in S3
+        Body: JSON.stringify(finalBucket[filename]),
+      };
+      s3.upload(params, function (err, data) {
+        if (err) {
+          throw err;
+        }
+        console.log(`File uploaded successfully. ${data.Location}`);
+      });
+    }
+  };
+
+  (async () => {
+    await sleep(2999); // blocking sleep function
+    downsample();
+    writeToS3();
+  })();
+})();
