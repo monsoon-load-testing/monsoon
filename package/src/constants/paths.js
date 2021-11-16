e1b110d7433bf3d583577ddcf0908052e7d38ae7
Minh Vu <minhphanh.vu@gmail.com>
2021-11-16 15:01:20 -0600

    Delete package from npm pack
    Co-authored-by: Stephanie Cunnane <stephanie.cunnane@gmail.com>
    Co-authored-by: Minh Vu <minhphanh.vu@gmail.com>
    Co-authored-by: Alex Drover <alexfdrover@gmail.com>
    Co-authored-by: Raul de Hevia <raul.dehevia@gmail.com>

--- package/src/constants/paths.js
+++
@@ -1,6 +0,0 @@
-const os = require("os");
-
-const MONSOON_GLOBAL_DIRECTORY = os.homedir() + "/.monsoon";
-const MONSOON_ENV_FILE_PATH = os.homedir() + "/.monsoon/.env";
-
-module.exports = { MONSOON_GLOBAL_DIRECTORY, MONSOON_ENV_FILE_PATH };
