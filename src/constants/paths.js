c09daa0d7aafbfb8d13617b1c2701bd6c929093a
Raul de Hevia <raul.dehevia@gmail.com>
2021-11-19 15:10:30 -0800

    START_HERE_REPO changed to https://github.com/monsoon-load-testing/monsoon_tests.git
    Co-authored-by: Minh Vu <minhphanh.vu@gmail.com>
    Co-authored-by: Stephanie Cunnane <stephanie.cunnane@gmail.com>
    Co-authored-by: Alex Drover <alexfdrover@gmail.com>

--- src/constants/paths.js
+++ src/constants/paths.js
@@ -3,7 +3,7 @@ const os = require("os");
 const MONSOON_GLOBAL_DIRECTORY = os.homedir() + "/.monsoon";
 const MONSOON_ENV_FILE_PATH = os.homedir() + "/.monsoon/.env";
 const START_HERE_REPO =
-  "https://github.com/monsoon-load-testing/start-here-test.git";
+  "https://github.com/monsoon-load-testing/monsoon_tests.git";
 
 module.exports = {
   MONSOON_GLOBAL_DIRECTORY,
